748fc9bf7f07bee2466811019d4e7cf20a7332c2
Fabricio Buzeto <nuk.anime13@gmail.com>
2015-04-11 16:44:56 -0300

    We have a board with dimensions and visual output

--- game_of_life.py
+++ game_of_life.py
@@ -1,17 +1,15 @@
 class GameOfLife(object):
   def __init__(self, rows, cols):
-    self.cols = cols
     self.rows = rows
+    self.cols = cols
+    pass
 
   def dimensions(self):
-    return [self.cols, self.rows]
+    return [self.rows,self.cols]
 
   def board(self):
-    linhas = []
-    coluna = []
-    for col in xrange(0,self.cols):
-      coluna.append(0)
-    for row in range(self.rows):
-      linhas.append(coluna)
-    return linhas
-
+    result = ""
+    for x in range(0,self.cols):
+      result += " "
+    return result + "\n"
+        
--- game_of_life_test.py
+++ game_of_life_test.py
@@ -1,24 +1,21 @@
 import unittest
 from game_of_life import GameOfLife
 
+
 class game_of_life_test(unittest.TestCase):
 
   def test_we_have_a_game(self):
-    game = GameOfLife(0,0)
-    self.assertEquals(game.dimensions(),[0,0])
-
-    game = GameOfLife(1,1)
-    self.assertEquals(game.dimensions(),[1,1])
-
-
-  def test_linha_morta (self):
-    game = GameOfLife(1,2)
-    self.assertEquals(game.board(),
-      [[0,0]]
-    )
-
-  def test_multilinha_morta (self):
     game = GameOfLife(2,1)
-    self.assertEquals(game.board(),
-      [[0],[0]]
-    )
+    self.assertEquals(game.dimensions(), [2,1])
+
+  def test_the_simplest_game_is_only_a_dead_cell(self):
+    game = GameOfLife(1,1)
+    self.assertEquals(game.board()," \n")  
+
+  def test_the_simplest_game_is_only_a_dead_row(self):
+    game = GameOfLife(1,2)
+    self.assertEquals(game.board(),"  \n")  
+
+  def xtest_the_game_starts_with_a_complete_dead_board(self):
+    game = GameOfLife(2,2)
+    self.assertEquals(game.board(),"  \n  \n")  
