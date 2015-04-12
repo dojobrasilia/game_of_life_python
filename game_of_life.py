e650f787dff1fb1911517ab5bc990df0682cd371
Fabricio Buzeto <nuk.anime13@gmail.com>
2015-04-11 21:14:32 -0300

    final

--- game_of_life.py
+++ game_of_life.py
@@ -2,14 +2,25 @@ class GameOfLife(object):
   def __init__(self, rows, cols):
     self.rows = rows
     self.cols = cols
+    self.board = self.createBoard()
     pass
 
   def dimensions(self):
     return [self.rows,self.cols]
 
-  def board(self):
+  def createBoard(self):
     result = ""
-    for x in range(0,self.cols):
-      result += " "
-    return result + "\n"
-        
+    for x in range(0,self.rows):
+      for y in range(0,self.cols):
+        result += " "
+      result += "\n"  
+    return result
+
+  def setAlive(self,row,col):
+    lines = self.board.split("\n") 
+    print(lines[col])
+    lines[col-1] = "x"
+    self.board = "\n".join(lines)
+    pass
+
+
--- game_of_life_test.py
+++ game_of_life_test.py
@@ -10,12 +10,22 @@ class game_of_life_test(unittest.TestCase):
 
   def test_the_simplest_game_is_only_a_dead_cell(self):
     game = GameOfLife(1,1)
-    self.assertEquals(game.board()," \n")  
+    self.assertEquals(game.board," \n")  
 
   def test_the_simplest_game_is_only_a_dead_row(self):
     game = GameOfLife(1,2)
-    self.assertEquals(game.board(),"  \n")  
+    self.assertEquals(game.board,"  \n")  
 
-  def xtest_the_game_starts_with_a_complete_dead_board(self):
+  def test_the_game_starts_with_a_complete_dead_board(self):
     game = GameOfLife(2,2)
-    self.assertEquals(game.board(),"  \n  \n")  
+    self.assertEquals(game.board,"  \n  \n") 
+
+  def test_the_simplest_game_is_only_a_live_cell(self):
+    game = GameOfLife(1,1)
+    game.setAlive(1,1);
+    self.assertEquals(game.board,"x\n")    
+
+  def test_the_simplest_game_is_only_a_live_cell(self):
+    game = GameOfLife(2,2)
+    game.setAlive(1,1);
+    self.assertEquals(game.board,"x \n  ")       
